258758d03fba697e133922b0f5cd6e3b20122b41
JazminCaceres <caceresjaz01@gmail.com>
2025-11-18 00:57:24 -0300

    Revert "nuevos archivos"
    This reverts commit 3c00f8bc3ca2fed7b0bf7c97bd787cff3294950f.

--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// admin/logout.php
-require_once '../includes/auth.php';
-logoutAdmin();
-header('Location: login.php?logged_out=1');
-exit;
-?>
--- generate_hash.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo password_hash('admin12345', PASSWORD_DEFAULT);
-?>
